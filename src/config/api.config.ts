9a680ed097ef90b5e9ca3e9432debfad5a952aeb
daniel.cordeiro <daniel.cordeiro@ladsistemas.com.br>
2018-05-29 21:57:47 -0300

    Tela de escolha de endereço

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-    baseUrl: "http://192.168.0.53:8080",
+    baseUrl: "http://localhost:8080",
     bucketBaseUrl: "https://s3-sa-east-1.amazonaws.com/dgc-curso-spring-ionic"
 
 }
